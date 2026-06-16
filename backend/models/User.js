cdbf8d2f47a60ba48ba81795893bcf01f0f13563
Dipika <maharjandipika49@gmail.com>
2026-06-16 09:33:39 +0545

    Added role to the User mode

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,17 +5,29 @@ const userSchema = mongoose.Schema(
     fullname: {
       type: String,
       required: true,
+      trim: true,
+      minlength: 2,
     },
 
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
     },
 
     password: {
       type: String,
       required: true,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
 
     resetPasswordToken: {
